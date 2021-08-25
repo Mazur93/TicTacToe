4019b46ae60c29bed7ceec780f17068bfff0d34d
Mazur93 <bartoszmazurkiewicz899@gmail.com>
2021-08-25 18:50:48 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,20 @@
+/* 
+ * File:   main.cpp
+ * Author: mazur
+ *
+ * Created on August 25, 2021, 6:39 PM
+ * A sample implementation to get familiar with C++;
+ */
+
+#include <cstdlib>
+
+using namespace std;
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return 0;
+}
+
